923fda6f5b9bd1fcb771845340fcf72a6620c85a
Saranya Balasubramanian <saranya.balasubramanian@oeaw.ac.at>
2025-08-12 10:28:45 +0200

    feat: include documentation

--- apis_ontology/settings/settings.py
+++ apis_ontology/settings/settings.py
@@ -6,3 +6,8 @@ BASE_DIR = Path(__file__).resolve()
 DEBUG = True
 APIS_BASE_URI = "https://apis-demo.acdh-dev.oeaw.ac.at/"
 ROOT_URLCONF = "apis_ontology.urls"
+ADDITIONAL_APPS = ["apis_core.documentation"]
+
+for app in ADDITIONAL_APPS:
+    if app not in INSTALLED_APPS:
+        INSTALLED_APPS.append(app)
