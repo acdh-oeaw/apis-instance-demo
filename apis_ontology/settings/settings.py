110b67724affb0650a68c5f2499f07e3b9becd12
Saranya Balasubramanian <saranya.balasubramanian@oeaw.ac.at>
2025-08-12 10:38:20 +0200

    feat: include cosmograph app

--- apis_ontology/settings/settings.py
+++ apis_ontology/settings/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve()
 DEBUG = True
 APIS_BASE_URI = "https://apis-demo.acdh-dev.oeaw.ac.at/"
 ROOT_URLCONF = "apis_ontology.urls"
-ADDITIONAL_APPS = ["apis_core.documentation"]
+ADDITIONAL_APPS = ["apis_core.documentation", "django_cosmograph"]
 
 for app in ADDITIONAL_APPS:
     if app not in INSTALLED_APPS:
