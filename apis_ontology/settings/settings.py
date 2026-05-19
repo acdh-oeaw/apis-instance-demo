b8593dcab72bb6b3875b06fdaa84874ca6d72546
Saranya Balasubramanian <saranya.balasubramanian@oeaw.ac.at>
2026-05-19 13:38:48 +0200

    feat(search): add search app to INSTALLED_APPS

--- apis_ontology/settings/settings.py
+++ apis_ontology/settings/settings.py
@@ -11,6 +11,7 @@ ADDITIONAL_APPS = [
     "apis_core.documentation",
     "django.contrib.staticfiles",
     "django_cosmograph",
+    "apis_core.search",
 ]
 CSP_DEFAULT_SRC = CSP_DEFAULT_SRC + (
     "'unsafe-eval'",  # needed for cosmograph 1.4.2
